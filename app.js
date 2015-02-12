9a38a8567b76cd198c42dfad9664f96f43499c2c
Andrew McGill <arm5077@gmail.com>
2015-02-12 15:52:52 -0500

    I am way behind in committing things. Everything has changed

--- app.js
+++ app.js
@@ -1,35 +1,87 @@
 app = angular.module("stewartApp", []);
 app.controller("guestsController", ["$scope", "$http", function($scope, $http){
 	
-	guests = [];
+	
+	$scope.selected = [];
 	
 	$http.get("data/guests.json").
 		success(function(data){
+			
 			$scope.guests = data;
+			$scope.categories = $scope.guests.map(function(guest){ return guest.category })
+				.filter(function(obj, i, a){ return a.indexOf(obj) == i })
+				.sort();
+			
+			$scope.selected = $scope.categories.slice(0);
+			
 		
-		
-			$scope.highlightPictures(["Senator", "Administration official"]);
-		
-		
-		
+			
 		
 		})
 		.error(function(err){
 			if(err) console.log(err);
 		});
+	
+	$scope.areAllSelected = function(){
+		return ( $scope.selected.length == $scope.categories.length ? true : false)
+	};
+	
+	$scope.selectOrUnselectAll = function(){
+		console.log($scope.selected.length);
+		if( $scope.selected.length ==  $scope.categories.length)
+			$scope.selected = [];
+		else 
+			$scope.selected = $scope.categories.slice(0);
 		
+		
+		$(".guestsContainer").isotope({
+			filter: function(){
+				return $scope.selected.indexOf($(this).attr("data-category")) != -1;
+			}
+		});
+		
+	};
+	
+	$scope.isCategorySelected = function(category){
+		return ($scope.selected.indexOf(category) == -1 ? false : true);
+	};
+	
 	$scope.getImageURL = function(img){
 		return {"background-image": 'url("img/' + img.replace(/ /g, "-") + '.jpg")'}
-	
-	}
+	};
 	
 	$scope.highlightPictures = function(categories) {
 		$scope.guests.forEach( function(guest){
-			if( categories.indexOf(guest["Political status"]) > -1 ) guest.selected = true;
+			if( categories.indexOf(guest.category) > -1 ) guest.selected = true;
 			else guest.selected = false;
 		});
 
-	}
+	};
+	
+	$scope.sortGuests = function(category){
+		
+		$(".guestsContainer").isotope({
+			itemSelector: '.guest',
+			layoutMode: 'fitRows',
+			getSortData: {
+				name: '[data-name]',
+				category: '[data-category]'
+			}	
+		});
+		
+		index = $scope.selected.indexOf(category);
+		if( index == -1 ) 
+			$scope.selected.push(category);
+		else
+			$scope.selected.splice(index, 1);
+		 
+		$(".guestsContainer").isotope({
+			filter: function(){
+				return $scope.selected.indexOf($(this).attr("data-category")) != -1;
+			}
+		});
+		
+	};
 	
 	
 	
@@ -43,5 +95,16 @@ app.directive('square', function() {
 
 		}
 	}
+});
+
+app.directive('isotope', function ($timeout) {
+    return {
+        link: function (scope, element, attrs) {
+            $timeout(function(){
+				
+				
+            });
+        }
+    };
 });
 
